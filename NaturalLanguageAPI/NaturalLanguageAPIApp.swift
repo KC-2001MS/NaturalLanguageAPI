3c06f3d546080d805071d841bb4ce8ca92bfcc0d
KC-2001MS <iroiro.work1234@gmail.com>
2023-07-04 15:34:22 +0900

    Initial Commit

---
+++ NaturalLanguageAPI/NaturalLanguageAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturalLanguageAPIApp.swift
+//  NaturalLanguageAPI
+//
+//  Created by 茅根啓介 on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct NaturalLanguageAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
